f71c367270c2a59fb0107b9374f85614d94ba8e6
Geolbaker <52758556+Geolbaker@users.noreply.github.com>
2026-02-20 13:15:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ import MacTheme from './components/global/main/themes/mac/body.js';
 import FourOneFour from './404.js';
 import './components/global/secrets.js';
 
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter>
-      <div id="home" className="h-screen">
-        <Routes>
-          <Route path="/" element={<Body />} />
-          <Route path="/xp" element={<XpTheme />} />
-          <Route path="/mac" element={<MacTheme />} />
-          <Route path="*" element={<FourOneFour />} />
-        </Routes>
-      </div>
-    </HashRouter>
+    <div id="home" className="h-screen">
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/xp" element={<XpTheme />} />
+        <Route path="/mac" element={<MacTheme />} />
+        <Route path="*" element={<FourOneFour />} />
+      </Routes>
+    </div>
   );
 }
 
